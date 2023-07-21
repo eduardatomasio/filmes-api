e350bc8bd529c495c18dc07bbdc6d51a69e8e365
Eduarda Tomasio <dudasoarestomasio@gmail.com>
2023-07-21 14:10:48 -0300

    validates parameters

--- Models/Filme.cs
+++ Models/Filme.cs
@@ -1,8 +1,18 @@
-﻿namespace FilmesAPI.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesAPI.Models;
 
 public class Filme
 {
+    [Required(ErrorMessage = "O título é um campo obrigatório")]
+    [MaxLength(300, ErrorMessage = "O campo título não pode exceder 300 caracteres")]
     public string Titulo { get; set; }
+    [Required(ErrorMessage = "O gênero é um campo obrigatório")]
+    [MaxLength (50, ErrorMessage = "O campo gênero não pode exceder 50 caracteres")]
+
     public string Genero { get; set; }
+    [Required(ErrorMessage = "A duração é um campo obrigatório")]
+    [Range (70, 600, ErrorMessage = "A duração do filme deve ter de 70 a 600 minutos")]
+
     public int Duracao { get; set; }
 }
