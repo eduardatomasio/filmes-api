f19efea88466a89d0cbff18fb3758723b1331ba0
Eduarda Tomasio <dudasoarestomasio@gmail.com>
2023-07-22 16:30:52 -0300

    creates first dto

--- Controllers/FilmeController.cs
+++ Controllers/FilmeController.cs
@@ -1,4 +1,6 @@
-﻿using FilmesAPI.Data;
+﻿using AutoMapper;
+using FilmesAPI.Data;
+using FilmesAPI.Dtos;
 using FilmesAPI.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Linq;
@@ -10,15 +12,18 @@ namespace FilmesAPI.Controllers;
 public class FilmeController : ControllerBase
 {
     private FilmeContext _context;
+    private IMapper _mapper;
 
-    public FilmeController(FilmeContext context)
+    public FilmeController(FilmeContext context, IMapper mapper)
     {
         _context = context;
+        _mapper = mapper;
     }
 
     [HttpPost]
-    public IActionResult AdicionarFilme([FromBody] Filme filme)
+    public IActionResult AdicionarFilme([FromBody] CreateFilmeDto filmeDto)
     {
+        Filme filme = _mapper.Map<Filme>(filmeDto);
         _context.Filmes.Add(filme);
         _context.SaveChanges();
         return CreatedAtAction(nameof(ExibirFilmePorId), new { id =  filme.Id }, filme);
---
+++ Dtos/CreateFilmeDto.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesAPI.Dtos;
+
+public class CreateFilmeDto
+{
+
+    [Required(ErrorMessage = "O título é um campo obrigatório")]
+    [StringLength(300, ErrorMessage = "O campo título não pode exceder 300 caracteres")]
+    public string Titulo { get; set; }
+    [Required(ErrorMessage = "O gênero é um campo obrigatório")]
+    [StringLength(50, ErrorMessage = "O campo gênero não pode exceder 50 caracteres")]
+
+    public string Genero { get; set; }
+    [Required(ErrorMessage = "A duração é um campo obrigatório")]
+    [Range(70, 600, ErrorMessage = "A duração do filme deve ter de 70 a 600 minutos")]
+
+    public int Duracao { get; set; }
+
+}
