ee37eea1401b1eaea7da0e59c37d5a7ddf27a03f
Eduarda Tomasio <dudasoarestomasio@gmail.com>
2023-07-22 15:49:09 -0300

    refactoring controller methods

--- Controllers/FilmeController.cs
+++ Controllers/FilmeController.cs
@@ -1,4 +1,5 @@
-﻿using FilmesAPI.Models;
+﻿using FilmesAPI.Data;
+using FilmesAPI.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Linq;
 
@@ -8,27 +9,31 @@ namespace FilmesAPI.Controllers;
 [Route("[controller]")]
 public class FilmeController : ControllerBase
 {
-    public static List<Filme> filmes = new List<Filme>();
-    public static int id = 0;
+    private FilmeContext _context;
+
+    public FilmeController(FilmeContext context)
+    {
+        _context = context;
+    }
 
     [HttpPost]
     public IActionResult AdicionarFilme([FromBody] Filme filme)
     {
-        filme.Id = id++;
-        filmes.Add(filme);
+        _context.Filmes.Add(filme);
+        _context.SaveChanges();
         return CreatedAtAction(nameof(ExibirFilmePorId), new { id =  filme.Id }, filme);
     }
 
     [HttpGet]
     public IEnumerable<Filme> ExibirFilmes([FromQuery] int skip = 0, [FromQuery] int take = 5)
     {
-        return filmes.Skip(skip).Take(take);
+        return _context.Filmes.Skip(skip).Take(take);
     }
 
     [HttpGet("{id}")]
     public IActionResult ExibirFilmePorId(int id)
     {
-        var filme =  filmes.FirstOrDefault(filme => filme.Id == id);
+        var filme =  _context.Filmes.FirstOrDefault(filme => filme.Id == id);
         if (filme == null) return NotFound();
         return Ok(filme);
     }
