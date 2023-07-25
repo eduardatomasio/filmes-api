f97ad5f203cc27e0c814e1cff13cb444560ec951
Eduarda Tomasio <dudasoarestomasio@gmail.com>
2023-07-25 18:25:59 -0300

    config entities

--- Dtos/Cinema/ReadCinemaDto.cs
+++ Dtos/Cinema/ReadCinemaDto.cs
@@ -1,8 +1,11 @@
-﻿namespace FilmesAPI.Dtos.Cinema
+﻿using FilmesAPI.Dtos.Endereco;
+
+namespace FilmesAPI.Dtos.Cinema
 {
     public class ReadCinemaDto
     {
         public int Id { get; set; }
         public string Nome { get; set; }
+        public ReadEnderecoDto ReadEnderecoDto { get; set; }
     }
 }
